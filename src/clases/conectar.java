df19c84700ef9a5d02cebb986a2a5b9fb42c2c16
Daniel Carbajal <Daniel Carbajal@192.168.1.76>
2018-02-23 21:00:36 -0600

    Version para probar Git

--- src/clases/conectar.java
+++ src/clases/conectar.java
@@ -1,8 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+//Hola esto es una prueba
+
+
 package clases;
 import java.sql.Connection;
 import java.sql.SQLException;
